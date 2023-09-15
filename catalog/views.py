bc5c3757240f36b177e8950e1700d3cc8cecbbe6
Acetonerer <bolokan2014@gmail.com>
2023-09-15 11:55:32 +0300

    initial commit

---
+++ catalog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.shortcuts import render
+import json
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        ph_number = request.POST.get("phone")
+        message = request.POST.get("message")
+        print(f'Новое сообщение от  {name}({ph_number}): {message}')
+    return render(request, 'catalog/contacts.html')

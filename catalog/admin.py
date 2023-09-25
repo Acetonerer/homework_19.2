cc0976f1336196c7a3eaad56427578d110b6897f
Acetonerer <bolokan2014@gmail.com>
2023-09-25 20:58:07 +0300

    update admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Contacts, Version
 
 
 @admin.register(Product)
@@ -8,8 +8,16 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('pk', 'product_name', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('product_name', 'descriptions',)
-
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('pk', 'category_name',)
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('contact_name', 'ph_number', 'message',)
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('product', 'number_version', 'name_version', 'is_active',)
+    list_filter = ('is_active', 'product',)

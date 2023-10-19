fb2659017cb3361913b4a7c78df3dd2e00546fe3
Acetonerer <bolokan2014@gmail.com>
2023-10-19 23:42:41 +0700

    fix views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,42 +1,41 @@
 from django.db import transaction
 from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView, CreateView, ListView, UpdateView
 from catalog.forms import ProductForm, VersionBaseInlineFormSet, VersionForm
-from catalog.models import Product, Contacts, Version
-
-
+from catalog.models import Product, Contacts, Category, Version
 class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:list')
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
 
 
 class ProductListView(ListView):
     model = Product
     content_object_name = 'page_obj'
     paginate_by = 3
-
     def get_queryset(self):
         return Product.objects.order_by('-date_create')
-
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
         context_data['title'] = 'Наши товары'
         return context_data
-
-
 class ProductUpdateWithVersionView(UpdateView):
     model = Product
     form_class = ProductForm
-
     def get_success_url(self, *args, **kwargs):
         return reverse('catalog:view', args=[self.object.id])
-
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
-
         VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1, formset=VersionBaseInlineFormSet)
         if self.request.method == 'POST':
             context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)
@@ -55,13 +54,10 @@ class ProductUpdateWithVersionView(UpdateView):
                 else:
                     return super().form_invalid(form)
         return super().form_valid(form)
-
-
 class ContactsCreateView(CreateView):
     model = Contacts
     template_name = "catalog/contacts.html"
     fields = ["contact_name", "ph_number", "message"]
-
     def post(self, request):
         contact_name = request.POST.get("name")
         ph_number = request.POST.get("phone")
@@ -72,11 +68,8 @@ class ContactsCreateView(CreateView):
             message=message
         )
         return HttpResponseRedirect(self.request.path_info)
-
-
 class ProductDetailViews(DetailView):
     model = Product
-
     def get_context_data(self, *args, **kwargs):
         context_data = super().get_context_data(*args, **kwargs)
         product_name = Product.objects.get(pk=self.kwargs.get('pk'))
